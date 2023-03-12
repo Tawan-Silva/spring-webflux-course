77ffa8636c785d210a727b64175e3fe9be1fd440
Tawan Silva <tawan.tls43@gmail.com>
2023-03-12 11:52:22 -0300

    add unit test to save user service

---
+++ src/test/java/br/com/tawandev/webfluxcourse/service/UserServiceTest.java
@@ -0,0 +1,67 @@
+package br.com.tawandev.webfluxcourse.service;
+
+import br.com.tawandev.webfluxcourse.entity.User;
+import br.com.tawandev.webfluxcourse.mapper.UserMapper;
+import br.com.tawandev.webfluxcourse.model.request.UserRequest;
+import br.com.tawandev.webfluxcourse.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.Objects;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository repository;
+
+    @Mock
+    private UserMapper mapper;
+
+    @InjectMocks
+    private UserService service;
+
+    @Test
+    void save() {
+        UserRequest request = new UserRequest("Tawan", "tawan.tls43@gmail.com", "12345678");
+        User entity = User.builder().build();
+
+        when(mapper.toEntity(any(UserRequest.class))).thenReturn(entity);
+        when(repository.save(any(User.class))).thenReturn(Mono.just(User.builder().build()));
+
+        Mono<User> result = service.save(request);
+
+        StepVerifier.create(result)
+                .expectNextMatches(Objects::nonNull)
+                .expectComplete()
+                .verify();
+
+        Mockito.verify(repository, times(1)).save(any(User.class));
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
