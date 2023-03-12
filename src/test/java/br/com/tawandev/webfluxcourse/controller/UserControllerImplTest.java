b421954aebe3a441b514eda704fdd219bfb2dbaa
Tawan Silva <tawan.tls43@gmail.com>
2023-03-12 17:32:26 -0300

    add integration test to findAll endpoint with success

--- src/test/java/br/com/tawandev/webfluxcourse/controller/UserControllerImplTest.java
+++ src/test/java/br/com/tawandev/webfluxcourse/controller/UserControllerImplTest.java
@@ -1,7 +1,9 @@
 package br.com.tawandev.webfluxcourse.controller;
 
 import br.com.tawandev.webfluxcourse.entity.User;
+import br.com.tawandev.webfluxcourse.mapper.UserMapper;
 import br.com.tawandev.webfluxcourse.model.request.UserRequest;
+import br.com.tawandev.webfluxcourse.model.response.UserResponse;
 import br.com.tawandev.webfluxcourse.service.UserService;
 import com.mongodb.reactivestreams.client.MongoClient;
 import org.junit.jupiter.api.DisplayName;
@@ -11,21 +13,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureWebTestClient
 class UserControllerImplTest {
 
+    public static final String ID = "12345";
+    public static final String NAME = "Tawan";
+    public static final String EMAIL = "tawan.tls43@gmail.com";
+    public static final String PASSWORD = "123";
     @Autowired
     private WebTestClient webTestClient;
 
@@ -35,15 +42,18 @@ class UserControllerImplTest {
     @MockBean
     private MongoClient mongoClient;
 
+    @MockBean
+    private UserMapper mapper;
+
     @Test
     @DisplayName("Test endpoint save with success")
     void testSaveWithSuccess() {
-        UserRequest request = new UserRequest("Tawan", "tawan.tls43@gmail.com", "123");
+        UserRequest request = new UserRequest(NAME, EMAIL, PASSWORD);
 
        when(service.save(any(UserRequest.class))).thenReturn(Mono.just(User.builder().build()));
 
        webTestClient.post().uri("/users")
-               .contentType(MediaType.APPLICATION_JSON)
+               .contentType(APPLICATION_JSON)
                .body(BodyInserters.fromValue(request))
                .exchange()
                .expectStatus().isCreated();
@@ -54,10 +64,10 @@ class UserControllerImplTest {
     @Test
     @DisplayName("Test endpoint save with bad request")
     void testSaveWithBadRequest() {
-        UserRequest request = new UserRequest(" Tawan", "tawan.tls43@gmail.com", "123");
+        UserRequest request = new UserRequest(NAME.concat(" "), EMAIL, PASSWORD);
 
         webTestClient.post().uri("/users")
-                .contentType(MediaType.APPLICATION_JSON)
+                .contentType(APPLICATION_JSON)
                 .body(BodyInserters.fromValue(request))
                 .exchange()
                 .expectStatus().isBadRequest()
@@ -71,11 +81,43 @@ class UserControllerImplTest {
     }
 
     @Test
-    void findById() {
+    @DisplayName("Test find by id endpoint with success")
+    void testFindByIdWithSuccess() {
+        final var userResponse = new UserResponse(ID, NAME, EMAIL, PASSWORD);
+
+        when(service.findById(anyString())).thenReturn(Mono.just(User.builder().build()));
+        when(mapper.toResponse(any(User.class))).thenReturn(userResponse);
+
+        webTestClient.get().uri("/users/" + "12345")
+                .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.id").isEqualTo(ID)
+                .jsonPath("$.name").isEqualTo(NAME)
+                .jsonPath("$.email").isEqualTo(EMAIL)
+                .jsonPath("$.password").isEqualTo(PASSWORD);
+
     }
 
     @Test
+    @DisplayName("Test find All endpoint with success")
     void findAll() {
+        final var userResponse = new UserResponse(ID, NAME, EMAIL, PASSWORD);
+
+        when(service.findAll()).thenReturn(Flux.just(User.builder().build()));
+        when(mapper.toResponse(any(User.class))).thenReturn(userResponse);
+
+        webTestClient.get().uri("/users")
+                .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.[0].id").isEqualTo(ID)
+                .jsonPath("$.[0].name").isEqualTo(NAME)
+                .jsonPath("$.[0].email").isEqualTo(EMAIL)
+                .jsonPath("$.[0].password").isEqualTo(PASSWORD);
+
     }
 
     @Test
